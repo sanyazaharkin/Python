0d1fecd3645698db6395c0fb61064d27f5568642
sanyazaharkin <sanyazaharkin@gmail.com>
2021-10-07 23:31:37 +0500

    create bot

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import telebot
+
+bot = telebot.TeleBot("2093362951:AAFuxsWHIXyzRWz9NTa1jHtfGTiyZ8xbsUU")
+
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+  if message.text == "Привет":
+      bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+  elif message.text == "/help":
+      bot.send_message(message.from_user.id, "Напиши Привет")
+  else:
+      bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        
